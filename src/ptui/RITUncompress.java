b98993b1f7734173008a9e7249ff8e47c168f8bc
nk6706 <nk6706@g.rit.edu>
2020-11-22 18:16:05 +0300

    Added compressed file and writing uncompressed file functionality. Error in finding file directory.

--- src/ptui/RITUncompress.java
+++ src/ptui/RITUncompress.java
@@ -2,24 +2,40 @@ package ptui;
 
 import model.RITQTCodec;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class RITUncompress {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.out.println("Usage: java RITUncompress compressed.rit uncompressed.txt");
             return;
         }
 
+        String inputFilename = args[0];
+        String outputFilename = args[1];
+
         RITQTCodec codec = new RITQTCodec();
         int[] dataArray = codec.decodeToArray(
-                RITUncompress.class.getResourceAsStream("/compressed/simple8x8-2.rit") );
+                RITUncompress.class.getResourceAsStream(inputFilename) );
 
-        for( int value : dataArray ) {
-            System.out.println(value);
+        FileWriter fileWriter = new FileWriter(outputFilename);//Error in writing file.
+        for(int value : dataArray){
+            fileWriter.write(value);
         }
+        fileWriter.close();
+
+        //Output
+        System.out.println("Input file: " + inputFilename);
+        for( int value : dataArray ) {
+            System.out.print(value + " ");
+        }
+
+        System.out.println("\nOutput File: " + outputFilename);
     }
 
 }
