32d89cd1fe55e1b6bb9ae2ec3d4463d3fec78f58
nk6706 <nk6706@g.rit.edu>
2020-11-22 17:55:42 +0300

    Added command line input.

--- src/gui/RITViewer.java
+++ src/gui/RITViewer.java
@@ -9,6 +9,8 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RITViewer extends Application {
@@ -19,23 +21,27 @@ public class RITViewer extends Application {
         //Set title
         stage.setTitle("RITViewer");
 
-        //earth256x256.txt
-        int imageWidth = 256;
-        int imageHeight = 256;
+        //get parameters
+        Parameters params = getParameters();
+        String fileName = params.getRaw().get(0);
 
-        URL url = RITViewer.class.getResource("/uncompressed/earth256x256.txt");
-        File file = new File(url.toURI());
-
-
+        System.out.println(fileName);
 
         //Load file into a scanner and generate a grayscale image from its data.
-        Scanner fileScanner = new Scanner( RITViewer.class.getResourceAsStream("/uncompressed/earth256x256.txt") );
-        ImageView imageView = new ImageView( generateGrayscaleImage(fileScanner, imageHeight, imageWidth) );
+        Scanner fileScanner = new Scanner( RITViewer.class.getResourceAsStream(fileName) );
+        List<Integer> inputList = new LinkedList<>();
+        while(fileScanner.hasNext()){
+            inputList.add(fileScanner.nextInt());
+        }
+        int imageWidth = (int) Math.sqrt(inputList.size());
+        int imageHeight = (int) Math.sqrt(inputList.size());
+        ImageView imageView = new ImageView( generateGrayscaleImage(inputList, imageWidth, imageHeight) );
+
 
         //Create basic scene for testing purposes.
         BorderPane borderPane = new BorderPane();
         borderPane.setCenter(imageView);
-        stage.setScene( new Scene(borderPane, 256, 256) );
+        stage.setScene( new Scene(borderPane, imageWidth, imageHeight) );
         stage.show(); //Display the stage.
 
     }
@@ -43,13 +49,13 @@ public class RITViewer extends Application {
         Application.launch(args);
     }
 
-    private Image generateGrayscaleImage(Scanner grayscaleValues, int imageWidth, int imageHeight) {
+    private Image generateGrayscaleImage(List<Integer> inputList, int imageWidth, int imageHeight) {
         WritableImage grayscaleImage = new WritableImage(imageWidth, imageHeight);
         PixelWriter pixelWriter = grayscaleImage.getPixelWriter();
         for( int y = 0; y < imageHeight; ++y ) {
             for ( int x = 0; x < imageWidth; ++x ) {
                 //Get pixel values from file one-by-one.
-                Color color = Color.grayRgb( grayscaleValues.nextInt() );
+                Color color = Color.grayRgb( inputList.get(y*imageHeight+x));
                 pixelWriter.setColor(x,y,color);
             }
         }
