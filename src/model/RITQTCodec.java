80e19b6f9e8c41a30987b3119963049248dd2f44
nk6706 <nk6706@rit.edu>
2020-11-29 09:12:33 -0500

    Compress implemented. Has fatal errors, debugging required.

--- src/model/RITQTCodec.java
+++ src/model/RITQTCodec.java
@@ -4,14 +4,138 @@ import model.exceptions.InvalidImageSpecificationException;
 import model.utils.Utils;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class RITQTCodec {
 
-    public RITQTNode encode(  ) {
+    public ArrayList<Integer> encodeToArray(InputStream resource ) {
         //TODO
-        return null;
+        Scanner fileScanner = new Scanner(resource);
+        ArrayList<Integer> dataArray = new ArrayList<>();
+
+        while (fileScanner.hasNext()){
+            dataArray.add(fileScanner.nextInt());
+        }
+
+        int size = dataArray.size();
+        int side = (int) Math.sqrt(size);
+
+        System.out.println(dataArray);
+
+        int[][] twoDArray = new int[side][side];
+
+        for(int i=0; i<side; i++){
+            for(int j=0; j<side;j++){
+                twoDArray[i][j] = dataArray.get(i*side+j);
+            }
+        }
+        System.out.println(side);
+
+        RITQTNode root = compress(side,0,0,twoDArray);
+
+
+        ArrayList<Integer> compressArray = new ArrayList<>();
+        compressArray.add(size);
+        inorder(root,compressArray);
+        System.out.println(compressArray);
+
+        return compressArray;
+    }
+
+    private RITQTNode parse(List<Integer> tokens) {
+        int val = tokens.remove(0);
+        if (val != -1) {
+            return new RITQTNode(val);
+        } else {
+            RITQTNode ul = parse(tokens);
+            RITQTNode ur = parse(tokens);
+            RITQTNode ll = parse(tokens);
+            RITQTNode lr = parse(tokens);
+            return new RITQTNode(val, ul, ur, ll, lr);
+        }
+    }
+
+
+
+    private ArrayList<Integer> inorder(RITQTNode ritqtNode, ArrayList<Integer> data){
+        if(ritqtNode!=null){
+            inorder(ritqtNode.getUpperLeft(),data);
+            inorder(ritqtNode.getUpperRight(),data);
+            inorder(ritqtNode.getLowerLeft(),data);
+            inorder(ritqtNode.getLowerRight(),data);
+            data.add(ritqtNode.getVal());
+        }
+        return data;
+    }
+
+    public RITQTNode compress(int side, int startRow, int startCol, int[][] dataArray){
+        System.out.println(side+"  "+ dataArray[startRow][startCol]);
+        if(side<=1){
+            return new RITQTNode(dataArray[startRow][startCol]);
+        }else{
+            int offset=side/2;
+            RITQTNode ul;
+            RITQTNode ur;
+            RITQTNode ll;
+            RITQTNode lr;
+            if(checkIfEqual(offset,startRow,startCol,dataArray)){
+                 ul = new RITQTNode(dataArray[startRow][startCol]);
+            }else {
+                offset=offset/2;
+                ul = new RITQTNode(-1,
+                        compress(offset,startRow,startCol,dataArray),
+                        compress(offset,startRow,startCol+offset,dataArray),
+                        compress(offset,startRow+offset,startCol,dataArray),
+                        compress(offset,startRow+offset,startCol+offset,dataArray));
+            }
+            if(checkIfEqual(offset,startRow,startCol+offset,dataArray)) {
+                ur = new RITQTNode(dataArray[startRow][startCol+offset]);
+            }else {
+                offset=offset/2;
+                ur = new RITQTNode(-1,
+                        compress(offset,startRow,startCol,dataArray),
+                        compress(offset,startRow,startCol+offset,dataArray),
+                        compress(offset,startRow+offset,startCol,dataArray),
+                        compress(offset,startRow+offset,startCol+offset,dataArray));
+            }
+            if(checkIfEqual(offset,startRow+offset,startCol,dataArray)){
+                ll = new RITQTNode(dataArray[startRow+offset][startCol]);
+            }else {
+                offset=offset/2;
+                ll = new RITQTNode(-1,
+                        compress(offset,startRow,startCol,dataArray),
+                        compress(offset,startRow,startCol+offset,dataArray),
+                        compress(offset,startRow+offset,startCol,dataArray),
+                        compress(offset,startRow+offset,startCol+offset,dataArray));
+            }
+            if(checkIfEqual(offset,startRow+offset,startCol+offset,dataArray)){
+                lr = new RITQTNode(dataArray[startRow+offset][startCol+offset]);
+            }else {
+                offset=offset/2;
+                lr = new RITQTNode(-1,
+                        compress(offset,startRow,startCol,dataArray),
+                        compress(offset,startRow,startCol+offset,dataArray),
+                        compress(offset,startRow+offset,startCol,dataArray),
+                        compress(offset,startRow+offset,startCol+offset,dataArray));
+            }
+
+            return new RITQTNode(-1,ul,ur,ll,lr);
+
+        }
+    }
+
+    public boolean checkIfEqual(int side, int startRow, int startCol,int[][] dataArray){
+        for(int i=0;i<side;i++){
+            for(int j=0;j<side;j++){
+                if(dataArray[startRow][startCol]!=dataArray[startRow+i][startCol+j]){
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 
     public int[][] decodeToArray( InputStream resource ) throws InvalidImageSpecificationException {
@@ -67,6 +191,5 @@ public class RITQTCodec {
                 }
             }
         }
-
     }
 }
--- src/ptui/RITCompress.java
+++ src/ptui/RITCompress.java
@@ -1,5 +1,11 @@
 package ptui;
 
+import model.RITQTCodec;
+import model.exceptions.InvalidImageSpecificationException;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class RITCompress {
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -7,6 +13,37 @@ public class RITCompress {
             //TODO
 
             return;
+        }
+        try {
+
+            //Loading files and such may throw IO exceptions which are handled below.
+            InputStream inputFile = new FileInputStream(args[0]);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+
+            RITQTCodec codec = new RITQTCodec();
+            //Codec takes InputStream as parameter and returns raw pixel data as a 2D-Array.
+            ArrayList<Integer> dataArray = codec.encodeToArray(  //this function throws an exception if the image size is
+                    // not a power of two.
+                    inputFile);
+
+//            for (int data : dataArray) {
+//                //for (int value : row) {
+//                    System.out.println(data);
+//                    writer.append(String.valueOf(data));
+//                    writer.newLine();
+//                //}
+//            }
+//
+//            writer.close();
+//            System.out.println("Output written to: " + args[1]);
+
+        } catch (IOException e) {
+            System.out.println("Please make sure that desired input and output files / their directories exist.");
+            System.out.println(e.toString());
+            e.printStackTrace();
+        } catch (InvalidImageSpecificationException e) {
+            System.out.println("The image specification is invalid -- image size is not a power of 2.");
+            e.printStackTrace();
         }
     }
 }
